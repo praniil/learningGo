ddf31a9fe605acc7eae8a56877fcd5152ab974a5
praniil <pparajuli15@gmail.com>
2023-12-25 16:40:23 +0545

    feat: added few properties like append and prepend in the list

--- DSA/linkedList.go
+++ DSA/linkedList.go
@@ -6,14 +6,14 @@ import (
 
 type Node struct {
 	data int
-	next *Node	//pointer to the next node
+	next *Node //pointer to the next node
 }
 type LinkedLis struct {
-	head *Node		//pointer to the head of the list
+	head *Node //pointer to the head of the list
 }
 
-func (li *LinkedLis) Insert(data int) {
-	newNode := &Node{ data: data, next: nil }
+func (li *LinkedLis) append(data int) {
+	newNode := &Node{data: data, next: nil}
 	if li.head == nil {
 		li.head = newNode
 		return
@@ -26,7 +26,26 @@ func (li *LinkedLis) Insert(data int) {
 	current.next = newNode
 }
 
-func (li *LinkedLis) Display () {
+func (li *LinkedLis) insertNext(data int, prevNode *Node) {
+	if prevNode == nil {
+		fmt.Println("Previous node cannot be nil")
+	}
+	newNode := &Node{data: data, next: nil}
+	newNode.next = prevNode.next
+	prevNode.next = newNode
+}
+
+func (li *LinkedLis) prepend(data int, nextNode *Node) {
+	if nextNode == nil {
+		fmt.Println("Next node cannot be nil")
+	}
+	newNode := &Node{data: data, next: nil}
+	newNode.next = li.head
+	li.head = newNode
+
+}
+
+func (li *LinkedLis) Display() {
 	current := li.head
 	for current != nil {
 		fmt.Printf("%d ->", current.data)
@@ -35,10 +54,20 @@ func (li *LinkedLis) Display () {
 	fmt.Println("Nil")
 }
 
-func LinkedList () {
+func LinkedList() {
 	fmt.Println("IN linked list")
 	li := LinkedLis{}
-	li.Insert(45);
-	li.Insert(30);
-	li.Display();
+	li.append(45)
+	li.append(30)
+	li.append(90)
+
+	currentNode := li.head
+	for currentNode != nil && currentNode.data != 90 {
+		currentNode = currentNode.next
+	}
+	if currentNode != nil {
+		li.prepend(200, currentNode)
+		li.insertNext(2, currentNode)
+	}
+	li.Display()
 }
